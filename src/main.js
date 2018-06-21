a396bd3ddfa1aea4d1bbd46518e0cde8c8387cfa
AlexisMunozv <alexismunozv@icloud.com>
2018-06-21 16:35:26 -0500

    Listado de alumnas

--- src/main.js
+++ src/main.js
@@ -1,118 +1,36 @@
 //En este caso puedes usar main.js para todo tu código que tenga que ver con mostrar los datos en la pantalla,
-const studentsButton=document.getElementById('submit-btn');
-const studentsContainer=document.getElementById('response-container');
+const SedeList=document.getElementById('Sede');
+const SedeContainer=document.getElementById('SedeContainer');
 
-studentsButton.addEventListener('click', function(e){
+SedeList.addEventListener('click', function(e){
 e.preventDefault();
-getStudents();
+SedeContainer.innerHTML = '';
+getSede();
 })
 
-function getStudents(){
+
+  function getSede(){
     const request = new XMLHttpRequest();
-    request.open('GET','../data/cohorts/lim-2018-03-pre-core-pw/users.json');
-    request.onload = addStudents;
+    request.open('GET','http://192.168.1.12:8887/users(1).json',true);
+    request.onload = addSede;
     request.onerror = handleError;
     request.send();
-    console.log(request.responseText);
-}
-function handleError (){
+   
+   function handleError (){
     console.log('error');
-}
+   }
 
-function addStudents (){
-    const data=JSON.parse(event.target.responseText);
-    for (let i=0; i<data.length; i++){
+   function addSede (){
+    const data=JSON.parse(this.responseText);
+      for (let i=0; i<data.length; i++){
         let li = document.createElement('li');
         li.innerText=data[i].name;
-        studentsContainer.appendChild(li);
-    }
-}
+        SedeContainer.appendChild(li);
+    };
+  
+    console.log();
+    
+}}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //Menu desplegable
-	let menuSede=document.getElementById("Sede1");
-	let menuGeneracion=document.getElementById("Generacion1");
-    let menuCursos=document.getElementById("Cursos1");
-    let menuAlumnas=document.getElementById("Alumnas1");
-
-   	
-   //Acción al hacer click a Sede de la lista desplegable
-	document.getElementById("Sede").addEventListener("click",()=>{
-			menuSede.style.display="block";
-			menuGeneracion.style.display="none";
-			menuCursos.style.display="none";			
-			menuAlumnas.style.display="none";
-    })
